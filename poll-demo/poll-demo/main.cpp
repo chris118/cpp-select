f73bea8725d10cefc25e5fe04f7d84a04779b801
xiaopeng wang <12405301@qq.com>
2020-10-16 16:36:13 +0800

    demo for pull

---
+++ poll-demo/poll-demo/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  poll-demo
+//
+//  Created by 王晓鹏 on 2020/10/15.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
